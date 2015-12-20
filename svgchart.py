9227209611047b5fa52ec88ad5c1dacc40dfc573
Jason Tatem <jtatemgithub@gmail.com>
2015-12-19 21:50:04 -0500

    Moved to dynamic tick spacing, bases off order of magnitude

--- svgchart.py
+++ svgchart.py
@@ -15,7 +15,6 @@ default_xlabel = ''
 default_textcolor = 'black'
 default_graphtitle = ''
 default_textsize = 12
-default_tickinterval = 100
 default_legend_enable = True
 default_ts_mode = False
 default_gridlines_enable = False
@@ -33,7 +32,7 @@ svg_rect_tag = '<rect x="{}" y="{}" width="{}" height="{}" stroke="{}" stroke-wi
 
 # Linechart takes a dict arranged as {'seriesname1': [(xval1, yval1), (xval2, yval2), ...], 'seriesname2': ...} and returns HTML SVG code for a line chart.  Several display options available, those should be self explanatory 
 
-def linechart(dataset, h=default_height, w=default_width, linew=default_linewidth, borderw=default_border_width, bordercolor=default_border_color, background=default_background, yvals=default_yvals, xvals=default_xvals, ylabel=default_ylabel, xlabel=default_xlabel, textcolor=default_textcolor, graphtitle=default_graphtitle, textsize=default_textsize, tickinterval=default_tickinterval, legend_enable=default_legend_enable, ts_mode=default_ts_mode, gridlines_enable=default_gridlines_enable):
+def linechart(dataset, h=default_height, w=default_width, linew=default_linewidth, borderw=default_border_width, bordercolor=default_border_color, background=default_background, yvals=default_yvals, xvals=default_xvals, ylabel=default_ylabel, xlabel=default_xlabel, textcolor=default_textcolor, graphtitle=default_graphtitle, textsize=default_textsize, legend_enable=default_legend_enable, ts_mode=default_ts_mode, gridlines_enable=default_gridlines_enable):
   linecolors=['#FF0000', '#00FF00', '#0000FF', '#FFF66', '#880000', '#FF00FF', '#008888', '#001188', '#FF5500', '#267326', '#80d5ff', '#990097']
   colorcycle = list(linecolors)
   output = svg_start_tag.format(h, w, '0', '0', w, h, svg_style_block.format(background, borderw, bordercolor))
@@ -58,7 +57,7 @@ def linechart(dataset, h=default_height, w=default_width, linew=default_linewidt
     y_bottom_offset += 40
   chartw = w - x_left_offset - x_right_offset
   charth = h - y_top_offset - y_bottom_offset
-  scaleddata = scaler(dataset, h=charth, w=chartw, target_interval=tickinterval)
+  scaleddata = scaler(dataset, h=charth, w=chartw)
   colormap = {}
   for series in sorted(scaleddata['series'].keys()):
     if len(colorcycle) == 0:
@@ -114,7 +113,7 @@ def linechart(dataset, h=default_height, w=default_width, linew=default_linewidt
 
 # Scaler transforms the dataset into the positional coordinate range of the chart area.  It also calls axisval to generate the x and y axis tick values and positional scaling.  This is a little messy as we're just passing the axis tick target_interval through like some sort of jerk.  This probably means I have chosen poorly in how I arranged these functions and I should probably revisit the workflow.
 
-def scaler(dataset, h=default_height, w=default_width, x_left_offset=0, x_right_offset=0, y_top_offset=0, y_bottom_offset=0, target_interval=default_tickinterval):
+def scaler(dataset, h=default_height, w=default_width, x_left_offset=0, x_right_offset=0, y_top_offset=0, y_bottom_offset=0):
   output = {'series':{}}
   true_xmin = None
   true_ymin = None
@@ -147,7 +146,7 @@ def scaler(dataset, h=default_height, w=default_width, x_left_offset=0, x_right_
   output['ymax'] = true_ymax
   xscale = float(w) / float((true_xmax - true_xmin))
   yscale = float(h) / float((true_ymax - true_ymin))
-  av = axisvals(true_ymin, true_ymax, true_xmin, true_xmax, h=h, w=w, target_interval=target_interval)
+  av = axisvals(true_ymin, true_ymax, true_xmin, true_xmax, h=h, w=w)
   output['xaxis'] = av['x']
   output['yaxis'] = av['y']
   for series in dataset:
@@ -170,18 +169,27 @@ def scaler(dataset, h=default_height, w=default_width, x_left_offset=0, x_right_
 
 # Axisvals generates the value and positions of the x and y axis tickmarks
 
-def axisvals(ymin, ymax, xmin, xmax, h=default_height, w=default_width, target_interval=default_tickinterval):
-  x_seg_count = w / target_interval
-  y_seg_count = h / target_interval
-#  print('{} {} {} {} {} {} {} {}'.format(ymin, ymax, xmin, xmax, h, w, x_seg_count, y_seg_count))
+def axisvals(ymin, ymax, xmin, xmax, h=default_height, w=default_width):
+  xspan = xmax - xmin
+  xmag = 0 
+  while xspan >= 10:
+    xspan = xspan / 10
+    xmag += 1
+  x_seg_count = (xmax - xmin) / 10 ** xmag  
+  yspan = ymax - ymin
+  ymag = 0
+  while yspan >= 10:
+    yspan = yspan / 10
+    ymag += 1
+  y_seg_count = (ymax - ymin) / 10 ** ymag
   xval_interval = float(xmax - xmin) / float(x_seg_count)
   yval_interval = float(ymax - ymin) / float(y_seg_count)
   xpos_interval = float(w) / float(x_seg_count)
   ypos_interval = float(h) / float(y_seg_count)
   output = {'x':[], 'y':[]}
-  for i in range(0, x_seg_count + 1):
+  for i in range(0, int(x_seg_count) + 1):
     output['x'].append((int(i * xval_interval) + xmin, int(i * xpos_interval)))
-  for i in range(0, y_seg_count + 1):
+  for i in range(0, int(y_seg_count) + 1):
     output['y'].append((int(i * yval_interval) + ymin, int(i * ypos_interval)))
   return output
   
